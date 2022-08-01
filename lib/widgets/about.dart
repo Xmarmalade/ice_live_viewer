80941b302e9985de599a6474de0184d1380f6ce0
iiijam <16839488+iiijam@users.noreply.github.com>
2022-08-01 17:50:33 +0800

    update version and test workflow

--- lib/widgets/about.dart
+++ lib/widgets/about.dart
@@ -14,7 +14,7 @@ class About extends StatelessWidget {
           showAboutDialog(
               context: context,
               applicationName: 'IceLiveViewer',
-              applicationVersion: 'beta v3',
+              applicationVersion: '0.1.5-beta1',
               applicationIcon: SizedBox(
                 width: 60,
                 child: Center(
