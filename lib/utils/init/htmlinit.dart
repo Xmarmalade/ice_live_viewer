06dc5faf534da7627222a435670ebcbc718501ff
iiijam <16839488+iiijam@users.noreply.github.com>
2022-09-17 09:49:02 +0800

    Fix WidgetsFlutterBinding error

--- lib/utils/init/htmlinit.dart
+++ lib/utils/init/htmlinit.dart
@@ -1,10 +1,17 @@
 import 'package:ice_live_viewer/main.dart';
 import 'package:flutter/material.dart';
 import 'package:ice_live_viewer/provider/themeprovider.dart';
+import 'package:ice_live_viewer/utils/prefs_helper.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void init() {
+void init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  PrefsHelper.prefs = await SharedPreferences.getInstance();
   runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_) => AppThemeProvider()),
+    ChangeNotifierProvider(
+      create: (_) => AppThemeProvider(),
+      lazy: false,
+    ),
   ], child: const MyApp()));
 }
